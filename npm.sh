4207e1ba5248fd91b84c77a28c4632819c858f67
guozaiss <492321329@qq.com>
2017-02-07 17:23:43 +0800

    shell down lib

---
+++ npm.sh
@@ -0,0 +1,21 @@
+echo "正在下载go lib"
+
+echo "github.com/astaxie/beego"
+go get github.com/astaxie/beego
+cd $GOPATH
+cd src/github.com/astaxie/beego
+git checkout v1.7.2
+
+echo "github.com/gorilla/mux"
+go get github.com/gorilla/mux
+cd $GOPATH
+cd src/github.com/gorilla/mux
+git checkout v1.3.0
+
+echo "github.com/go-sql-driver/mysql"
+go get github.com/go-sql-driver/mysql
+cd $GOPATH
+cd src/github.com/go-sql-driver/mysql
+git checkout v1.3
+
+echo "下载完毕"
