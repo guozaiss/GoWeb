5483e4f1a86ab137e51c64b473a504c2c9243506
guozaiss <492321329@qq.com>
2017-02-09 18:34:21 +0800

    modify mysql logic

--- src/controllers/queryPerson.go
+++ src/controllers/queryPerson.go
@@ -15,19 +15,17 @@ type queryPersonController struct {
 
 func QueryPerson(w http.ResponseWriter, r *http.Request) {
 	DealBegin(w, r)
-	//rows := dbUtils.Query("select * from showalltb_meizhi")
 	engine, err := xorm.NewEngine("mysql", "guo:123456@/meizhi")
 	if err!=nil {
 		fmt.Println(err)
 	}
-	//engine.Sync2(new(models.Meizhi))
-
 	ss,err :=engine.Query("select * from tb_meizhi where id ='id_01'")
 	fmt.Println(ss)
 	status,err :=engine.Insert(models.Meizhi{Id:"ididid",FileName:"FilefileName"})
 	fmt.Println(status)
 
 	rows := dbUtils.Query("select * from tb_meizhi where id ='id_01'")
+	rows.Columns()
 	for rows.Next() {
 		var id, fileName string
 		rows.Columns()
